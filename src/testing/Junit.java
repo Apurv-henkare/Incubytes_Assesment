2b4e25ed70ac95607058294fb782e9f09bd1e581
Apurv Henkare <apurvhenakare@gmail.com>
2022-09-07 23:03:58 +0530

    singleStringreturnsInteger

--- src/testing/Junit.java
+++ src/testing/Junit.java
@@ -2,13 +2,28 @@ package testing;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class Junit {
+	
+	private StringCalculator obj;
+	
+	@Before
+	public void init()
+	{
+		obj=new StringCalculator();
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void emptyStringTestReturnsZero() {
+		
+		assertEquals(obj.calculate(""),0);
+	} 
+	
+	@Test
+	public void singleStringreturnsInteger() {
+		assertEquals(obj.calculate("100"),100);
 	}
 
 }
--- src/testing/StringCalculator.java
+++ src/testing/StringCalculator.java
@@ -1,5 +1,25 @@
 package testing;
 
 public class StringCalculator {
+	
+	public int calculate(String input)
+	{
+		if(isEmpty(input))
+		{
+			return 0;
+		}
+		return Integer.parseInt(input);
+	}
+	
+	private boolean isEmpty(String input)
+	{
+		int len=input.length();
+		
+		if(len == 0)
+		{
+			return true;
+		}
+		return false;
+	}
 
 }
